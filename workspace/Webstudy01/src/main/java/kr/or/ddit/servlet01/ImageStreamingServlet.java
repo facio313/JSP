f1aa5ea49b9e7bb99bd1672299919d37bfb3a8d8
facio313 <faccio313@gmail.com>
2022-11-29 19:09:01 +0900

    20221129

---
+++ workspace/Webstudy01/src/main/java/kr/or/ddit/servlet01/ImageStreamingServlet.java
@@ -0,0 +1,104 @@
+package kr.or.ddit.servlet01;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.*;
+
+import java.util.Date;
+
+public class ImageStreamingServlet extends HttpServlet{
+	private String imageFolder;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		imageFolder = config.getInitParameter("imageFolder");
+		System.out.printf("받은 파라미터 : %s\n", imageFolder);
+	}
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		// 목록 보여주기
+		StringBuffer content = new StringBuffer();
+		
+		File file = new File("D:/contents/images");
+		File[] files = file.listFiles();
+
+		content.append("<html>\n");
+		content.append("<body>\n");
+		for (File f : files) {
+			if (f.isFile()) {
+				content.append(String.format("<a>%s</a><br>",f.getName()));
+			} else {
+				content.append("파일이 없습니다.");
+			}
+		}
+		content.append("<script>\n");
+		content.append("");
+		content.append("</script>\n");
+		content.append("</body>\n");
+		content.append("</html>\n");
+		
+		PrintWriter out = resp.getWriter();
+		out.println(content);
+		out.close();
+
+		// 사진 보여주기
+		ServletContext application = getServletContext();	
+		String imageName = req.getParameter("image");
+		String mimeType = application.getMimeType(imageName);
+		resp.setContentType(mimeType);
+		
+		File imageFile = new File(imageFolder, imageName);
+
+		FileInputStream fis = null;
+		OutputStream os = null;
+		try {
+			fis = new FileInputStream(imageFile);
+			os = resp.getOutputStream();
+			int tmp = -1;
+			while((tmp = fis.read()) != -1){
+				os.write(tmp);
+			}
+		} finally {
+			if (fis != null)
+			fis.close();
+			if (os != null)
+			os.close();
+		}
+		
+		
+		
+		
+//		ServletContext application = getServletContext(); // 가장 먼저 생성되고, 가장 오래 살아남는! 거기다 범위가 가장 넓은 저장소
+//		
+//		String imageName = req.getParameter("image");
+//		if (imageName == null || imageName.isEmpty()) {
+//			resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+//			return;
+//		}
+//		String mimeType = application.getMimeType(imageName);
+//		resp.setContentType(mimeType);
+//		
+//		File imageFile = new File(imageFolder, imageName);
+//		if(!imageFile.exists()) {
+//			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+//			return;
+//		}
+//		// try catch가 없을 때, while문이 돌아가다 예외가 발생하면 close가 안 되고, 다른 사람이 자원을 쓸 수 없음
+//		FileInputStream fis = null;
+//		OutputStream os = null;
+//		try {
+//			fis = new FileInputStream(imageFile);
+//			os = resp.getOutputStream();
+//			int tmp = -1;
+//			while((tmp = fis.read()) != -1){
+//				os.write(tmp);
+//			}
+//		} finally {
+//			if (fis != null)
+//			fis.close();
+//			if (os != null)
+//			os.close();
+//		}
+	}
+}
