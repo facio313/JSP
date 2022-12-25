9847bafbaa18a2e9cf9e7afed57d1c4b1bb056b8
facio313 <faccio313@gmail.com>
2022-12-25 23:43:00 +0900

    20221225 from mac

---
+++ workspace/Webstudy01/src/main/java/kr/or/ddit/servlet09/PooledObjectFactory.java
@@ -0,0 +1,11 @@
+package kr.or.ddit.servlet09;
+
+public interface PooledObjectFactory<T> {
+//	PooledObject<T>activateObject(PooledObject<T>);
+//	destroyObject(PooledObject<T>);
+//	destroyObject(PooledObject<T>, DestoryMode);
+//	makeOjbect();
+//	passivateObject(PooledObject<T>);
+//	validateObject(PooledObject<T>;)
+}
+
